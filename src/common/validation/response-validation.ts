d7801e11b84f6ed9c93ba7129ff127bc2a6d0ea4
HU SANG MIN <40601526+ViGilanteAF@users.noreply.github.com>
2025-08-07 02:39:34 +0900

    feat: validation 추가
    Request, Response 에 대한 Validation 추가

---
+++ src/common/validation/response-validation.ts
@@ -0,0 +1,35 @@
+import {
+  CallHandler,
+  ExecutionContext,
+  Injectable,
+  InternalServerErrorException,
+  NestInterceptor,
+} from '@nestjs/common';
+import { Observable, switchMap } from 'rxjs';
+
+@Injectable()
+export class ResponseValidationInterceptor<T extends object>
+  implements NestInterceptor<any, T>
+{
+  constructor(private readonly responseModel: new () => T) {}
+
+  intercept(_: ExecutionContext, next: CallHandler): Observable<T> {
+    return next.handle().pipe(
+      switchMap(async (data) => {
+        const transformedData = plainToInstance(this.responseModel, data, {
+          excludeExtraneousValues: true,
+        });
+        try {
+          await validateOrReject(transformedData, { stopAtFirstError: true });
+
+          return transformedData;
+        } catch (err) {
+          throw new InternalServerErrorException(
+            'response validation failed',
+            err,
+          );
+        }
+      }),
+    );
+  }
+}
