654165c01e71ac87db474eee2509bca7fb704ad5
HU SANG MIN <40601526+ViGilanteAF@users.noreply.github.com>
2025-08-07 02:41:20 +0900

    feat: health check 기능 추가
    Health check을 위한 기능 추가

---
+++ src/common/common.module.ts
@@ -0,0 +1,23 @@
+import { RequestValidationPipe } from './validation/request-validation';
+import { APP_FILTER, APP_PIPE } from '@nestjs/core';
+import { HttpExceptionFilter } from './exception/http-exception.filter';
+import { Logger, MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { HealthController } from './health/health.controller/health.controller';
+
+@Module({
+  imports: [TerminusModule, PrometheusModule.register()],
+  controllers: [HealthController, HealthController],
+  providers: [
+    Logger,
+    { provide: APP_PIPE, useClass: RequestValidationPipe },
+    { provide: APP_FILTER, useClass: HttpExceptionFilter },
+  ],
+})
+export class CommonModule implements NestModule {
+  configure(consumer: MiddlewareConsumer): any {
+    if (process.env.NODE_ENV != Environment.Test) {
+      consumer.apply(LoggingMiddleware).forRoutes('*');
+      consumer.apply(MetricMiddleware).exclude('/metrics').forRoutes('*');
+    }
+  }
+}
