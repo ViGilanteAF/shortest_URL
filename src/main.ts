70f5c4a29b7eb6f7fc25b8b2fb2ca6f203414a2e
HU SANG MIN <40601526+ViGilanteAF@users.noreply.github.com>
2025-08-11 03:46:54 +0900

    fix: import config 변경 및 port 번호 안보이도록 수정

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,18 @@
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { serverConfig } from './config/server.config';
+import { setUpSwagger } from './common/swagger/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { logger: winstonLogger });
+  const config = app.get(ConfigService);
+
   app.setGlobalPrefix('/api/v1');
   app.enableCors();
   app.use(helmet());
   setUpSwagger(app);
 
-  const PORT = app.get(serverConfig.KEY).port;
-  await app.listen(3000);
+  const PORT = app.get(ConfigService);
+  await app.listen(config.get('port'));
 }
 bootstrap();
