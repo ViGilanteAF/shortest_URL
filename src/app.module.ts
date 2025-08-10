e182136e51e4e068fd6133ccbbb1e417c487b30f
HU SANG MIN <40601526+ViGilanteAF@users.noreply.github.com>
2025-08-11 03:47:13 +0900

    fix: imports 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CommonModule } from './common/common.module';
+import { HealthModule } from './common/health/health.module';
+import { ConfigModule } from './config/config.module';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule, CommonModule, HealthModule],
   controllers: [AppController],
   providers: [AppService],
 })
