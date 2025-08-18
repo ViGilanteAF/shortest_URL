88b2067156b851b40ade14552ddbcc85f29eff37
HU SANG MIN <40601526+ViGilanteAF@users.noreply.github.com>
2025-08-19 02:09:09 +0900

    fix: 잘못된 DI 수정

--- src/counter/count-command.ts
+++ src/counter/count-command.ts
@@ -1,11 +1,11 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
-import { ShortestUrlCommandPort } from '../shortest-url/port/out/shortest-url-command';
+import { ShortestUrlCommand } from '../shortest-url/port/out/shortest-url-command';
 import { CountEntity } from './entity/count.entity';
 
 @Injectable()
-export class CountCommand implements ShortestUrlCommandPort {
+export class CountCommand implements ShortestUrlCommand {
   constructor(
     @InjectModel(CountEntity.name)
     private readonly countModel: Model<CountEntity>,
