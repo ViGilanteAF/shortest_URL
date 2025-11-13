4575082007c1fc192db1573456890ab3f893602a
HU SANG MIN <40601526+ViGilanteAF@users.noreply.github.com>
2025-11-13 23:41:52 +0900

    fix: url 생성 count 조회 및 증가, type 에러 방어적으로 변경

--- src/shortest-url/create-shortest-url/shortest-url.count.adapter.ts
+++ src/shortest-url/create-shortest-url/shortest-url.count.adapter.ts
@@ -1,42 +1,27 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
-import { Mutex } from '../../common/util/mutex.util';
 
 import { CountEntity } from '../../counter/entity/count.entity';
 import { LoadUpdateCountPort } from '../port/out/load-update-count.port';
 
 @Injectable()
 export class CountAdapter implements LoadUpdateCountPort {
-  static COUNT_RANGE = 10000;
-  private readonly mutex = new Mutex();
-  private readonly count = {
-    first: 0,
-    current: 0,
-    end: 0,
-  };
   constructor(
     @InjectModel(CountEntity.name)
     private readonly countModel: Model<CountEntity>,
   ) {}
 
   async findCountIncrease(): Promise<number> {
-    await this.mutex.lock();
+    const count = await this.countModel.findOneAndUpdate(
+      {},
+      { $inc: { current: 1 } },
+    );
 
-    if (this.count.current === this.count.end) {
-      const currentCount = await this.countModel.findOneAndUpdate(
-        {},
-        { $inc: { current: CountAdapter.COUNT_RANGE } },
-      );
-      this.count.first = currentCount!.current;
-      this.count.current = this.count.first;
-      this.count.end = this.count.first + CountAdapter.COUNT_RANGE;
-    } else {
-      this.count.current++;
+    if (!count) {
+      throw new Error('Count document not found');
     }
 
-    this.mutex.unlock();
-
-    return this.count.current;
+    return count.current;
   }
 }
