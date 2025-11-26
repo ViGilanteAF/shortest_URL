53497072acd138b5353a92dacd914a7a41706e04
HU SANG MIN <40601526+ViGilanteAF@users.noreply.github.com>
2025-11-27 02:47:00 +0900

    feat: async-mutex 추가

--- src/common/util/mutex.util.ts
+++ src/common/util/mutex.util.ts
@@ -1,32 +1,3 @@
-type ResolveFunction = () => void;
+import { Mutex as AsyncMutex } from 'async-mutex';
 
-export class Mutex {
-  private locked: boolean;
-  private waitingQueue: ResolveFunction[];
-
-  constructor() {
-    this.locked = false;
-    this.waitingQueue = [];
-  }
-
-  async lock(): Promise<void> {
-    if (this.locked) {
-      await new Promise((resolve) =>
-        this.waitingQueue.push(<ResolveFunction>resolve),
-      );
-    } else {
-      this.locked = true;
-    }
-  }
-
-  unlock(): void {
-    if (this.waitingQueue.length > 0) {
-      const nextResolve = this.waitingQueue.shift();
-      if (nextResolve) {
-        nextResolve();
-      }
-    } else {
-      this.locked = false;
-    }
-  }
-}
+export class Mutex extends AsyncMutex {}
--- src/shortest-url/create-shortest-url/shortest-url.count.adapter.ts
+++ src/shortest-url/create-shortest-url/shortest-url.count.adapter.ts
@@ -7,21 +7,42 @@ import { LoadUpdateCountPort } from '../port/out/load-update-count.port';
 
 @Injectable()
 export class CountAdapter implements LoadUpdateCountPort {
+  static COUNT_RANGE: 10000;
+
+  private mutex = new Mutex();
+  private count = {
+    first: 0,
+    current: 0,
+    end: 0,
+  };
+
   constructor(
     @InjectModel(CountEntity.name)
     private readonly countModel: Model<CountEntity>,
   ) {}
 
   async findCountIncrease(): Promise<number> {
-    const count = await this.countModel.findOneAndUpdate(
-      {},
-      { $inc: { current: 1 } },
-    );
-
-    if (!count) {
-      throw new Error('Count document not found');
+    const releas = await this.mutex.acquire();
+    try {
+      if (this.count.current === this.count.end) {
+        await this.setCount();
+      } else {
+        this.count.current++;
+      }
+    } finally {
+      releas();
     }
 
-    return count.current;
+    return this.count.current;
+  }
+
+  private async setCount() {
+    const count = await this.countModel.findOneAndUpdate(
+      {},
+      { $inc: { current: CountAdapter.COUNT_RANGE } },
+    );
+    this.count.first = count!.current;
+    this.count.current = this.count.first;
+    this.count.end = this.count.first + CountAdapter.COUNT_RANGE;
   }
 }
