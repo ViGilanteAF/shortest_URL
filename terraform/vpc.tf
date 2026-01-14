850376240d206ae0621f1f1594e2871f1b514ffa
HU SANG MIN <40601526+ViGilanteAF@users.noreply.github.com>
2026-01-15 01:55:58 +0900

    feat: terraform vpc추가

---
+++ terraform/vpc.tf
@@ -0,0 +1,29 @@
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0./16"
+}
+
+resource "aws_subnet" "public" {
+  vpc_id                  = aws_vpc.main.id
+  count                   = 2
+  cidr_block              = cidrsubnet("10.0.0.0/16", 8, count.index)
+  availability_zone       = ["ap-northeast-2a", "ap-northeast-2c"][count.index]
+  map_public_ip_on_launch = true
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+}
+
+resource "aws_route_table_association" "public" {
+  count          = 2
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public.id
+}
