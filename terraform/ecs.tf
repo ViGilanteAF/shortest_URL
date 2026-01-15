eca1fd04a03e776a3b93991d26be2cee21035977
HU SANG MIN <40601526+ViGilanteAF@users.noreply.github.com>
2026-01-15 11:19:53 +0900

    feat: terraform ecs추가

---
+++ terraform/ecs.tf
@@ -0,0 +1,59 @@
+resource "aws_ecs_cluster" "cluster" {
+  name = "shortest-url-cluster"
+}
+
+resource "aws_ecs_task_definition" "task" {
+  family                   = "shortest-url"
+  requires_compatibilities = ["FARGATE"]
+  network_mode             = "awsvpc"
+  cpu                      = "256"
+  memory                   = "512"
+  execution_role_arn       = aws_iam_role.ecs_task_executino.arn
+
+  container_definitions = jsonencode([{
+    name  = "app"
+    image = var.container_image
+    portMappings = [{
+      containerPort = var.container_port
+    }]
+  }])
+}
+
+resource "aws_security_group" "ecs_sg" {
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    from_port       = 8000
+    to_port         = 8000
+    protocol        = "tcp"
+    security_groups = [aws_security_group.alb_sg.id]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_ecs_service" "service" {
+  name            = "shortest-url-service"
+  cluster         = aws_ecs_cluster.cluster.id
+  task_definition = aws_ecs_task_definition.task.arn
+  launch_type     = "FARGATE"
+  desired_count   = 1
+
+  network_configuration {
+    subnets         = aws_subnet.public[*].id
+    security_groups = [aws_security_group.ecs_sg.id]
+  }
+
+  load_balancer {
+    target_group_arn = aws_lb_target_group.tg.arn
+    container_name   = "app"
+    container_port   = 8000
+  }
+
+  depends_on = [aws_lb_listener.listener]
+}
